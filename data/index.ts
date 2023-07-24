226dee55c2d73ba19f68eb4b220ec2122f5da046
Niall Quirke <quirkeniall@gmail.com>
2023-07-24 16:28:58 +0100

    copy update

--- data/index.ts
+++ data/index.ts
@@ -31,7 +31,7 @@ export const testimonials: Testimonial[] = [
     company: "Amazon.com",
     companyLink: "https://amazon.com",
     image: "/img/stephen.jpg",
-    comment: "Niall added a lot of value and innovation to our team.",
+    comment: "Niall added a lot of value and innovation to our team at AWS.",
     details:
       "Designing large distributed cloud infrastructure systems for enterprise clients and developed our teams tooling. My biggest achievement was dreaming up, designing, building and owning a tool that completely replaced our teams CLI client with a much faster, simpler and more secure browser client. This is now used daily by 500+ operation engineers in 8 sites globally.",
   },
