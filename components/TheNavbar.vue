75f5c08185c4c9d7d3f60f42d10af8d96aae4edd
Niall Quirke <quirkeniall@gmail.com>
2023-08-15 16:04:14 +0100

    copy update

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -21,7 +21,7 @@ const mobileMenuOpen = ref(false);
             What They Say About Niall
           </h1>
           <h2 class="text-gray-200 text-sm">
-            Ex-Amazon, Ex-Microsoft Senior Fullstack Software Engineer
+            Sustainability Communication Expert and Fullstack Software Engineer
           </h2>
         </div>
       </div>
